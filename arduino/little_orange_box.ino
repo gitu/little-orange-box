8db3de9574b91d712f7cd8f49b148e1aed786f29
Florian Schrag <f@schr.ag>
2023-08-04 23:39:28 +0200

    much cleaner solution

--- arduino/little_orange_box.ino
+++ arduino/little_orange_box.ino
@@ -9,14 +9,17 @@
 #define BUTTON_PIN GPIO_NUM_1
 #define MAX_IDLE_MS 1000*60*2
 
-Bounce debouncer = Bounce();
+Bounce b = Bounce();
 BleGamepad bleGamepad("Little Orange Box", "flo");
 BleGamepadConfiguration bleGamepadConfig;
 Adafruit_NeoPixel pixels(1, PIXEL, NEO_GRB + NEO_KHZ800);
 
 uint8_t curr_val = 0;
 
-unsigned long lastInteraction = 0;
+unsigned long nextSwitch = 1000;
+unsigned long currentInterval = 1000;
+bool currentlyOn = false;
+bool currentTarget = false;
 
 void setup() {
   pinMode(PIXEL_POWER, OUTPUT);
@@ -29,58 +32,88 @@ void setup() {
   pixels.show();
   delay(1000);
 
-  bleGamepadConfig.setButtonCount(1);
-  bleGamepadConfig.setHatSwitchCount(0);
-  bleGamepadConfig.setWhichSpecialButtons(false,false,false,false,false,false,false,false);
+  bleGamepad.begin(); // Begin the gamepad
 
-  bleGamepad.begin(&bleGamepadConfig); // Begin the gamepad
+  b.attach(BUTTON_PIN, INPUT);
+  b.interval(25);
 
-
-  debouncer.attach(BUTTON_PIN); // After setting up the button, setup the Bounce instance :
-  debouncer.interval(5);        // interval in ms
-
+  pixels.clear();
 }
 
-void loop() {
-  pixels.clear();
+bool initialConnection = false;
+bool launched = false;
 
+void loop() {
   if (bleGamepad.isConnected())
   {
-      debouncer.update(); // Update the Bounce instance
+    if (!initialConnection) {
+      pixels.setPixelColor(0, pixels.Color(0,0,255));
+      pixels.show();
+      initialConnection = true;
+    }
 
-      int value = debouncer.read(); // Get the updated value
 
-      // Press/release gamepad button and turn on or off the LED as determined by the state
-      if (value == LOW)
-      {
-          lastInteraction = millis();
-          pixels.setPixelColor(0, pixels.Color(0,0,255));
-          pixels.show();
-          delay(2);
+      b.update();
+
+      if (b.rose()){
+        nextSwitch = 1000;
+        currentInterval = 1000;
+        currentTarget = true;
+        currentlyOn = false;
+
+        bleGamepad.press(BUTTON_1);
+        bleGamepad.release(BUTTON_2);
+        launched = false;
+      }
+
+      if (b.fell()) {
           bleGamepad.release(BUTTON_1);
-      }
-      else
-      {
-          lastInteraction = millis();
-          pixels.setPixelColor(0, pixels.Color(255,0,0));
-          pixels.show();
-          delay(2);
-          bleGamepad.press(BUTTON_1);
+          if (!launched) {
+            bleGamepad.release(BUTTON_2);
+            pixels.setPixelColor(0, pixels.Color(0,0,255));
+            pixels.show();
+          }
       }
 
+      if (b.read() == HIGH) {
+        if (!launched) {
+          if (b.currentDuration()>nextSwitch) {
+            nextSwitch += currentInterval;
+            currentTarget = !currentTarget;
+            currentInterval = (2*currentInterval)/3;
 
+            if (currentInterval<20) {
+              bleGamepad.press(BUTTON_2);
+              launched = true;
+              currentTarget = true;
+            }
+          }
+          if (currentTarget != currentlyOn) {
+            if (currentTarget) {
+                pixels.setPixelColor(0, pixels.Color(255,0,0));
+            } else {
+                pixels.setPixelColor(0, pixels.Color(0,0,0));
+            }
+            pixels.show();
+            currentlyOn = currentTarget;
+          }
+        }
+      }
   } else {
-    for(long pixelHue = 0; pixelHue < 65536 && !bleGamepad.isConnected(); pixelHue += 256) {
+    initialConnection = false;
+    for(long pixelHue = 0; pixelHue < 65536; pixelHue += 256) {
       pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(pixelHue)));
       pixels.show();
       delay(20);
+      if (bleGamepad.isConnected()) {
+        pixelHue = 65536;
+      }
     }
-  }
 
-  if (millis() - lastInteraction > MAX_IDLE_MS) {
-     digitalWrite(PIXEL_POWER, LOW);
-     esp_sleep_enable_ext0_wakeup(PIXEL,1);
-     esp_deep_sleep_start();
-
+    if (millis() > MAX_IDLE_MS) {
+       digitalWrite(PIXEL_POWER, LOW);
+       esp_sleep_enable_ext0_wakeup(BUTTON_PIN, 1);
+       esp_deep_sleep_start();
+    }
   }
 }
